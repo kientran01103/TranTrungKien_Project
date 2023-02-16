b8b5c46e8c8d0e8004edfadf8b9a389029d11393
kientran01103 <125584160+kientran01103@users.noreply.github.com>
2023-02-17 00:00:42 +0700

    Update and rename README.md to TranTrungKien_App.java

--- README.md
+++
@@ -1,1 +0,0 @@
-# TranTrungKien_Project
---
+++ TranTrungKien_App.java
@@ -0,0 +1,6 @@
+public class TranTrungKien_App {
+       public static void main(String[] args) {
+              System.out.println("Tran Trung Kien hoc java.");
+       }
+  
+}
